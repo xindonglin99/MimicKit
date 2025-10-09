2a5e82f52352dcb24474f3c47e8c2d113830ab34
Jason Peng <japeng@nvidia.com>
2025-10-09 09:59:42 -0700

    cleaning up steering env

--- mimickit/envs/task_steering_env.py
+++ mimickit/envs/task_steering_env.py
@@ -8,14 +8,15 @@ import util.torch_util as torch_util
 class TaskSteeringEnv(amp_env.AMPEnv):
     def __init__(self, config, num_envs, device, visualize):
         env_config = config["env"]
-        self._enable_rand_tar_dir = env_config.get("enable_rand_tar_dir", True)
+        self._rand_tar_dir = env_config.get("rand_tar_dir", True)
+        self._rand_face_dir = env_config.get("rand_face_dir", True)
         self._tar_speed_min = env_config["tar_speed_min"]
         self._tar_speed_max = env_config["tar_speed_max"]
         self._tar_change_time_min = env_config["tar_change_time_min"]
         self._tar_change_time_max = env_config["tar_change_time_max"]
 
-        self._reward_steering_dir_w = float(env_config["reward_steering_dir_w"])
-        self._reward_steering_facing_w = float(env_config["reward_steering_facing_w"])
+        self._reward_steering_tar_w = float(env_config["reward_steering_tar_w"])
+        self._reward_steering_face_w = float(env_config["reward_steering_face_w"])
 
         self._reward_steering_vel_scale = float(env_config["reward_steering_vel_scale"])
         self._reward_steering_tangent_scale = float(env_config["reward_steering_tangent_scale"])
@@ -26,8 +27,8 @@ class TaskSteeringEnv(amp_env.AMPEnv):
     
     def _build_envs(self, config, num_envs):
         if (self._visualize):
-            self._marker_ids = []
-            self._facing_marker_ids = []
+            self._tar_marker_ids = []
+            self._face_marker_ids = []
 
         super()._build_envs(config, num_envs)
         return
@@ -38,20 +39,20 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         num_envs = self.get_num_envs()
         self._tar_speed = torch.ones([num_envs], device=self._device, dtype=torch.float)
         self._tar_dir = torch.zeros([num_envs, 2], device=self._device, dtype=torch.float)
-        self._tar_facing_dir = torch.zeros([num_envs, 2], device=self._device, dtype=torch.float)
+        self._face_dir = torch.zeros([num_envs, 2], device=self._device, dtype=torch.float)
         self._tar_change_times = torch.zeros([num_envs], device=self._device, dtype=torch.float)
         self._prev_root_pos = torch.zeros([num_envs, 3], device=self._device, dtype=torch.float)
         
         self._tar_dir[..., 0] = 1.0
-        self._tar_facing_dir[..., 0] = 1.0
+        self._face_dir[..., 0] = 1.0
 
         return
     
-    def _get_marker_id(self):
-        return self._marker_ids[0]
+    def _get_tar_marker_id(self):
+        return self._tar_marker_ids[0]
     
-    def _get_facing_marker_id(self):
-        return self._facing_marker_ids[0]
+    def _get_face_marker_id(self):
+        return self._face_marker_ids[0]
 
     def _pre_physics_step(self, actions):
         super()._pre_physics_step(actions)
@@ -69,14 +70,14 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         tar_dist_max = 2.0
 
         char_id = self._get_char_id()
-        marker_id = self._get_marker_id()
-        facing_marker_id = self._get_facing_marker_id()
+        tar_marker_id = self._get_tar_marker_id()
+        face_marker_id = self._get_face_marker_id()
         
         root_pos = self._engine.get_root_pos(char_id)
         root_pos = root_pos[env_ids]
         tar_speed = self._tar_speed[env_ids]
         tar_dir = self._tar_dir[env_ids]
-        tar_facing_dir = self._tar_facing_dir[env_ids]
+        face_dir = self._face_dir[env_ids]
 
         tar_dist = (tar_speed - self._tar_speed_min) / (self._tar_speed_max - self._tar_speed_min)
         tar_dist = (tar_dist_max - tar_dist_min) * tar_dist + tar_dist_min
@@ -91,25 +92,25 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         tar_axis[..., -1] = 1.0
         marker_rot = torch_util.axis_angle_to_quat(tar_axis, tar_theta)
         
-        facing_marker_pos = root_pos.clone()
-        facing_marker_pos[..., 0:2] += tar_dist_min * tar_facing_dir
-        facing_marker_pos[..., 2] = 0.0
+        face_marker_pos = root_pos.clone()
+        face_marker_pos[..., 0:2] += tar_dist_min * face_dir
+        face_marker_pos[..., 2] = 0.01
 
-        facing_theta = torch.atan2(tar_facing_dir[..., 1], tar_facing_dir[..., 0])
-        facing_axis = torch.zeros_like(root_pos)
-        facing_axis[..., -1] = 1.0
-        facing_marker_rot = torch_util.axis_angle_to_quat(tar_axis, facing_theta)
+        face_theta = torch.atan2(face_dir[..., 1], face_dir[..., 0])
+        face_axis = torch.zeros_like(root_pos)
+        face_axis[..., -1] = 1.0
+        face_marker_rot = torch_util.axis_angle_to_quat(tar_axis, face_theta)
 
 
-        self._engine.set_root_pos(env_ids, marker_id, marker_pos)
-        self._engine.set_root_rot(env_ids, marker_id, marker_rot)
-        self._engine.set_root_vel(env_ids, marker_id, 0.0)
-        self._engine.set_root_ang_vel(env_ids, marker_id, 0.0)
+        self._engine.set_root_pos(env_ids, tar_marker_id, marker_pos)
+        self._engine.set_root_rot(env_ids, tar_marker_id, marker_rot)
+        self._engine.set_root_vel(env_ids, tar_marker_id, 0.0)
+        self._engine.set_root_ang_vel(env_ids, tar_marker_id, 0.0)
 
-        self._engine.set_root_pos(env_ids, facing_marker_id, facing_marker_pos)
-        self._engine.set_root_rot(env_ids, facing_marker_id, facing_marker_rot)
-        self._engine.set_root_vel(env_ids, facing_marker_id, 0.0)
-        self._engine.set_root_ang_vel(env_ids, facing_marker_id, 0.0)
+        self._engine.set_root_pos(env_ids, face_marker_id, face_marker_pos)
+        self._engine.set_root_rot(env_ids, face_marker_id, face_marker_rot)
+        self._engine.set_root_vel(env_ids, face_marker_id, 0.0)
+        self._engine.set_root_ang_vel(env_ids, face_marker_id, 0.0)
         
         return
     
@@ -137,12 +138,15 @@ class TaskSteeringEnv(amp_env.AMPEnv):
     def _reset_task(self, env_ids):
         n = len(env_ids)
 
-        if (self._enable_rand_tar_dir):
+        if (self._rand_tar_dir):
             rand_theta = 2 * np.pi * torch.rand(n, device=self._device) - np.pi
-            rand_face_theta = 2 * np.pi * torch.rand(n, device=self._device) - np.pi
         else:
             rand_theta = torch.zeros(n, device=self._device)
-            rand_face_theta = torch.zeros(n, device=self._device)
+
+        if (self._rand_face_dir):
+            rand_face_theta = 2 * np.pi * torch.rand(n, device=self._device) - np.pi
+        else:
+            rand_face_theta = rand_theta.clone()
 
         tar_dir = torch.stack([torch.cos(rand_theta), torch.sin(rand_theta)], dim=-1)
         tar_speed = (self._tar_speed_max - self._tar_speed_min) * torch.rand(n, device=self._device) + self._tar_speed_min
@@ -153,7 +157,7 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         
         self._tar_speed[env_ids] = tar_speed
         self._tar_dir[env_ids] = tar_dir
-        self._tar_facing_dir[env_ids] = face_tar_dir
+        self._face_dir[env_ids] = face_tar_dir
         self._tar_change_times[env_ids] = self._time_buf[env_ids] + rand_dt
         
         if (self._visualize):
@@ -168,15 +172,15 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         root_rot = self._engine.get_root_rot(char_id)
         tar_dir = self._tar_dir
         tar_speed = self._tar_speed
-        tar_face_dir = self._tar_facing_dir
+        face_dir = self._face_dir
 
         if (env_ids is not None):
             root_rot = root_rot[env_ids]
             tar_dir = tar_dir[env_ids]
             tar_speed = tar_speed[env_ids]
-            tar_face_dir = tar_face_dir[env_ids]
+            face_dir = face_dir[env_ids]
         
-        task_obs = compute_steering_observations(root_rot, tar_dir, tar_speed, tar_face_dir)
+        task_obs = compute_steering_observations(root_rot, tar_dir, tar_speed, face_dir)
         obs = torch.cat([obs, task_obs], dim=-1)
 
         return obs
@@ -191,12 +195,12 @@ class TaskSteeringEnv(amp_env.AMPEnv):
                                                       root_rot=char_root_rot,
                                                       tar_dir=self._tar_dir, 
                                                       tar_speed=self._tar_speed,
-                                                      tar_face_dir=self._tar_facing_dir,
+                                                      face_dir=self._face_dir,
                                                       dt=self._engine.get_timestep(),
                                                       vel_err_scale=self._reward_steering_vel_scale,
                                                       tangent_err_scale=self._reward_steering_tangent_scale,
-                                                      dir_reward_w=self._reward_steering_dir_w,
-                                                      facing_reward_w=self._reward_steering_facing_w)
+                                                      tar_reward_w=self._reward_steering_tar_w,
+                                                      face_reward_w=self._reward_steering_face_w)
         return
     
     def _update_misc(self):
@@ -224,17 +228,17 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         super()._ig_build_env(env_id, config)
         
         if (self._visualize):
-            marker_id, facing_marker_id = self._ig_build_markers(env_id)
+            tar_marker_id, face_marker_id = self._ig_build_markers(env_id)
 
             if (env_id == 0):
-                self._marker_ids.append(marker_id)
-                self._facing_marker_ids.append(facing_marker_id)
+                self._tar_marker_ids.append(tar_marker_id)
+                self._face_marker_ids.append(face_marker_id)
             else:
-                marker_id0 = self._marker_ids[0]
-                facing_marker_id0 = self._facing_marker_ids[0]
+                tar_marker_id0 = self._tar_marker_ids[0]
+                face_marker_id0 = self._face_marker_ids[0]
 
-                assert(marker_id0 == marker_id)
-                assert(facing_marker_id0 == facing_marker_id)
+                assert(tar_marker_id0 == tar_marker_id)
+                assert(face_marker_id0 == face_marker_id)
 
         return
     
@@ -243,23 +247,23 @@ class TaskSteeringEnv(amp_env.AMPEnv):
         col_filter = 0
         segmentation_id = 0
         
-        marker_id = self._engine.create_actor(env_id=env_id, 
+        tar_marker_id = self._engine.create_actor(env_id=env_id, 
                                              asset=self._marker_asset, 
-                                             name="marker", 
+                                             name="tar_marker", 
                                              col_group=col_group, 
                                              col_filter=col_filter, 
                                              segmentation_id=segmentation_id,
                                              color=[0.8, 0.0, 0.0])
 
-        facing_marker_id = self._engine.create_actor(env_id=env_id, 
+        face_marker_id = self._engine.create_actor(env_id=env_id, 
                                              asset=self._marker_asset, 
-                                             name="facing_marker", 
+                                             name="face_marker", 
                                              col_group=col_group, 
                                              col_filter=col_filter, 
                                              segmentation_id=segmentation_id,
                                              color=[0.0, 0.8, 0.0])
 
-        return marker_id, facing_marker_id
+        return tar_marker_id, face_marker_id
 
     
 
@@ -268,7 +272,7 @@ class TaskSteeringEnv(amp_env.AMPEnv):
 #####################################################################
 
 @torch.jit.script
-def compute_steering_observations(root_rot, tar_dir, tar_speed, tar_face_dir):
+def compute_steering_observations(root_rot, tar_dir, tar_speed, face_dir):
     # type: (Tensor, Tensor, Tensor, Tensor) -> Tensor
     
     tar_dir3d = torch.cat([tar_dir, torch.zeros_like(tar_dir[..., 0:1])], dim=-1)
@@ -278,43 +282,43 @@ def compute_steering_observations(root_rot, tar_dir, tar_speed, tar_face_dir):
     local_tar_dir = local_tar_dir[..., 0:2]
     tar_speed = tar_speed.unsqueeze(-1)
     
-    tar_face_dir3d = torch.cat([tar_face_dir, torch.zeros_like(tar_face_dir[..., 0:1])], dim=-1)
-    local_tar_face_dir = torch_util.quat_rotate(heading_rot, tar_face_dir3d)
-    local_tar_face_dir = local_tar_face_dir[..., 0:2]
+    face_dir3d = torch.cat([face_dir, torch.zeros_like(face_dir[..., 0:1])], dim=-1)
+    local_face_dir = torch_util.quat_rotate(heading_rot, face_dir3d)
+    local_face_dir = local_face_dir[..., 0:2]
 
-    obs = torch.cat([local_tar_dir, tar_speed, local_tar_face_dir], dim=-1)
+    obs = torch.cat([local_tar_dir, tar_speed, local_face_dir], dim=-1)
 
     return obs
 
 
-#@torch.jit.script
-def compute_steering_reward(root_pos, prev_root_pos, root_rot, tar_dir, tar_speed, tar_face_dir, dt,
-                           vel_err_scale, tangent_err_scale, dir_reward_w, facing_reward_w):
+@torch.jit.script
+def compute_steering_reward(root_pos, prev_root_pos, root_rot, tar_dir, tar_speed, face_dir, dt,
+                           vel_err_scale, tangent_err_scale, tar_reward_w, face_reward_w):
     # type: (Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, float, float, float, float, float) -> Tensor
 
     delta_root_pos = root_pos - prev_root_pos
     root_vel = delta_root_pos / dt
-    tar_dir_speed = torch.sum(tar_dir * root_vel[..., :2], dim=-1)
+    char_tar_speed = torch.sum(tar_dir * root_vel[..., :2], dim=-1)
 
-    tar_dir_vel = tar_dir_speed.unsqueeze(-1) * tar_dir
-    tar_vel_err = tar_speed - tar_dir_speed
+    char_tar_vel = char_tar_speed.unsqueeze(-1) * tar_dir
+    tar_vel_err = tar_speed - char_tar_speed
     tar_vel_err = torch.square(tar_vel_err)
 
-    tangent_vel = root_vel[..., :2] - tar_dir_vel
+    tangent_vel = root_vel[..., :2] - char_tar_vel
     tangent_vel_err = torch.sum(torch.square(tangent_vel), dim=-1)
 
-    dir_reward = torch.exp(-vel_err_scale * tar_vel_err - tangent_err_scale * tangent_vel_err)
+    tar_reward = torch.exp(-vel_err_scale * tar_vel_err - tangent_err_scale * tangent_vel_err)
 
-    speed_mask = tar_dir_speed <= 0
-    dir_reward[speed_mask] = 0
+    speed_mask = char_tar_speed <= 0
+    tar_reward[speed_mask] = 0
 
     heading_rot = torch_util.calc_heading_quat(root_rot)
-    facing_dir = torch.zeros_like(root_pos)
-    facing_dir[..., 0] = 1.0
-    facing_dir = torch_util.quat_rotate(heading_rot, facing_dir)
-    facing_err = torch.sum(tar_face_dir * facing_dir[..., 0:2], dim=-1)
-    facing_reward = torch.clamp_min(facing_err, 0.0)
+    char_face_dir = torch.zeros_like(root_pos)
+    char_face_dir[..., 0] = 1.0
+    char_face_dir = torch_util.quat_rotate(heading_rot, char_face_dir)
+    face_err = torch.sum(face_dir * char_face_dir[..., 0:2], dim=-1)
+    face_reward = torch.clamp_min(face_err, 0.0)
 
-    reward = dir_reward_w * dir_reward + facing_reward_w * facing_reward
+    reward = tar_reward_w * tar_reward + face_reward_w * face_reward
 
     return reward
